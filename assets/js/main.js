3bc9d46f1521a222ee4a4274713d0fd691fbd84b
astargeneraltrading <goldendaysa@yahoo.com>
2019-11-17 16:02:11 +0800

    fix fast contact

--- assets/js/main.js
+++ assets/js/main.js
@@ -163,13 +163,13 @@
   teamMember()
 
   // Scroll to top active js
-  $(window).on('scroll', function () {
-    if ($(this).scrollTop() > 600) {
-      $('.scroll-top').removeClass('not-visible')
-    } else {
-      $('.scroll-top').addClass('not-visible')
-    }
-  })
+  // $(window).on('scroll', function () {
+  //   // if ($(this).scrollTop() > 600) {
+  //   //   $('.scroll-top').removeClass('not-visible')
+  //   // } else {
+  //   //   $('.scroll-top').addClass('not-visible')
+  //   // }
+  // })
   $('.scroll-top').on('click', function (event) {
     $('html,body').animate({
         scrollTop: 0
